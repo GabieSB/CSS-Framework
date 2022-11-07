851abab1dabc4d51ae4d10bcd4bb27fdb563f3ea
Gerardo Araya <gerardo.araya.ceciliano@est.una.ac.cr>
2022-11-06 23:23:45 -0600

    modificaciones
    en caso de conflicto descartar mi parte

---
+++ recurso.js
@@ -0,0 +1,26 @@
+    function move() {
+        var elem = document.getElementById("myBar");   
+        var width = 1;
+        var id = setInterval(frame, 10);
+        function frame() {
+          if (width >= 100) {
+            clearInterval(id);
+          } else {
+            width++; 
+            elem.style.width = width + '%'; 
+          }
+        }
+      }
+      function openCity(evt, cityName) {
+        var i, x, tablinks;
+        x = document.getElementsByClassName("city");
+        for (i = 0; i < x.length; i++) {
+          x[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablink");
+        for (i = 0; i < x.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+        }
+        document.getElementById(cityName).style.display = "block";
+        evt.currentTarget.className += " w3-red";
+      }
